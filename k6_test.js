9f31b58146e964b054f1b21bf46e2985bd8395cd
DESKTOP-93VP4M1 <cyberdinet800@yahoo.co.jp>
2022-11-09 18:39:44 +0900

    221109

--- k6_test.js
+++ k6_test.js
@@ -4,32 +4,13 @@ import http from 'k6/http';
 import { check, fail } from 'k6';
 import { sleep } from 'k6';
 
-const URL = 'https://test01.megakiku.net/api/settings/sync';
-//const URL = 'https://mc.megakiku.net/api/settings/sync';
-const ACCESS_TOKEN = "l989sumy0evz9076d74h5uxn"; // test01
+//const ACCESS_TOKEN = "l989sumy0evz9076d74h5uxn"; // test01
 //const ACCESS_TOKEN = "l9peznle000829c772ear2x5"; // test11
-//const ACCESS_TOKEN = "l1ohlzt7000103f1c781ut8m"; // mc3
-
-
-const header = {
+const ACCESS_TOKEN = "l1ohlzt7000103f1c781ut8m"; // mc3
+let header = {
   "Content-Type": "application/json",
   "Authorization": ACCESS_TOKEN,
 };
-const body = {
-  "type": "6",  // 取得データのタイプ(2~8)
-  "spot_id": null,
-  "gateway_id": null,
-  "device_id": null,
-  "data_id": null,
-  "updated_at": null,
-  "current_page": null,
-  "state": "0,1",
-  "page_count": null // 1ページあたりデータ数
-};
-const params = {
-  headers: header
-};
-const querystring = "?state="+body["state"]+"&type="+body["type"];
 
 export let options = {
 /*
@@ -39,18 +20,119 @@ export let options = {
     { target: 30, duration: '1s' }
   ]
 */
-  vus: 10,
+//  vus: 10,
 //  iterations: 10,
-  duration: '10s',
+//  duration: '10s',
 };
 
-export default function () {
-  const response = http.get(URL+querystring, params);
+export function setup() {
+  return login_ui();
+//  return newdata_api();
+//  return login_api();
+//  return sync_api();
+}
+
+export default function (data) {
+  const response = http.request(data.method, data.URL+data.querystring, data.body, data.params);
   const checkResult = check(response, {
     "response code was 200": (response) => response.status == 200,
   });
   if (!checkResult) {
     fail("status code was not 200");
   }
-//  sleep(1);
+  sleep(1);
+}
+
+// **************************************************************
+// ローカル関数
+// **************************************************************
+
+/**
+ * ログイン画面へのアクセス
+ * @return {object} HTTPリクエストパラメータ
+ */
+ function login_ui () {
+  return {
+    method: 'GET',
+    URL: 'https://mc.megakiku.net/login',
+    params: null,
+    querystring: null,
+    body: null
+  };
+}
+
+/**
+ * 最新データAPIへのアクセス
+ * @return {object} HTTPリクエストパラメータ
+ */
+ function newdata_api () {
+  const body = {
+    "spot_id": 41,
+    "device_id": null,
+    "data_id": null,
+    "current_page": null,
+    "page_count": null // 1ページあたりデータ数(未設定の場合100)
+  };
+
+  return {
+    method: 'GET',
+    URL: 'https://mc.megakiku.net/api/newdata/tm2',
+    params: {
+      headers: header
+    },
+    querystring: "?spot_id="+body["spot_id"],
+    body: null
+  };
+}
+
+/**
+ * ログインAPIへのアクセス
+ * @return {object} HTTPリクエストパラメータ
+ */
+ function login_api () {
+  header = {
+    "Content-Type": "application/json"
+  };
+  const body = {
+    "login_id": "loaduser",
+    "password": "loaduser",
+  };
+
+  return {
+    method: 'POST',
+    URL: 'https://mc.megakiku.net/api/login',
+    params: {
+      headers: header
+    },
+    querystring: "",
+    body: JSON.stringify(body)
+  };
+}
+
+/**
+ * データ同期APIへのアクセス
+ * @return {object} HTTPリクエストパラメータ
+ */
+ function sync_api () {
+  const body = {
+    "type": "6",  // 取得データのタイプ(2~8)
+    "spot_id": null,
+    "gateway_id": null,
+    "device_id": null,
+    "data_id": null,
+    "updated_at": null,
+    "current_page": null,
+    "state": "0,1",
+    "page_count": null // 1ページあたりデータ数
+  };
+
+  return {
+    method: 'GET',
+    URL: 'https://mc.megakiku.net/api/settings/sync',
+    params: {
+      headers: header
+    },
+    querystring: "?type="+body["type"]+"&state="+body["state"],
+    body: null
+  };
 }
